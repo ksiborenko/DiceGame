c21a74b51e52f20bf5bee3a941a951b8b4d9b587
konrad <ksiborenko@gmail.com>
2021-04-26 18:45:36 +0200

    completed

--- script.js
+++ script.js
@@ -1,39 +1,42 @@
 'use strict';
 
-const playerZero = document.querySelector(`.player--0`);
-const playerOne = document.querySelector(`.player--1`);
-const dice = document.querySelector(`.dice`);
+const playerOne = document.querySelector(`.player--0`);
+const playerTwo = document.querySelector(`.player--1`);
+const dice = document.getElementById(`dice`);
 const rollDice = document.querySelector(`.btn--roll`);
-const hold = document.querySelector(`.btn--hold`);
+const hold = document.getElementById(`btn--hold`);
 const newGame = document.querySelector(`.btn--new`);
 
-let score, currentScore, currentPlayer, activeGame;
+let activePlayer = 0;
+let currentScore = 0;
+const score = [0, 0];
+let noWinner = true;
 
-const beginningSetting = function() {
-    dice.classList.add(`hidden`);
-    currentScore = 0;
-    currentPlayer = 0;
-    activeGame = true;
-    score = [0, 0];
-    playerZero.classList.add(`player--active`);
-    playerOne.classList.remove(`player--active`);
-    for (let i = 0; i < 2; i++) {
-        document.querySelector(`.player--${i}`).classList.remove(`player--winner`);
-        document.querySelector(`#score--${i}`).textContent = score[i];
-        document.getElementById(`current--${i}`).textContent = 0;
+const startingConditions = () => {
+    for (let x = 0; x < 2; x++) {
+        score[x] = 0;
+        document.getElementById(`score--${x}`).textContent = score[x];
+        document.getElementById(`current--${x}`).textContent = score[x];
+        document.querySelector(`.player--${x}`).classList.remove(`player--winner`);
     }
+    currentScore = 0;
+    activePlayer = 0;
+    dice.classList.add(`hidden`);
+    playerOne.classList.add(`player--active`);
+    playerTwo.classList.remove(`player--active`);
+    noWinner = true;
 };
 
-beginningSetting();
+startingConditions();
 
-rollDice.addEventListener(`click`, function() {
-    if (activeGame) {
-        dice.classList.remove(`hidden`);
+rollDice.addEventListener('click', function() {
+    if (noWinner) {
         let random = Math.trunc(Math.random() * 6) + 1;
+        dice.classList.remove(`hidden`);
         dice.src = `dice-${random}.png`;
         if (random !== 1) {
             currentScore += random;
-            document.getElementById(`current--${currentPlayer}`).textContent = currentScore;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
         } else {
             switchPlayer();
         }
@@ -41,25 +44,30 @@ rollDice.addEventListener(`click`, function() {
 });
 
 hold.addEventListener(`click`, function() {
-    if (activeGame) {
-        score[currentPlayer] += currentScore;
-        document.getElementById(`score--${currentPlayer}`).textContent = score[currentPlayer];
-        if (score[currentPlayer] >= 100) {
-            document.querySelector(`.player--${currentPlayer}`).classList.add(`player--winner`);
-            activeGame = false;
+    if (noWinner) {
+        score[activePlayer] += currentScore;
+        document.getElementById(`score--${activePlayer}`).textContent = score[activePlayer];
+        if (score[activePlayer] >= 10) {
+            document.querySelector(`.player--${activePlayer}`).classList.add(`player--winner`);
+            noWinner = false;
         } else {
             switchPlayer();
         }
     }
 });
 
-newGame.addEventListener(`click`, beginningSetting);
+newGame.addEventListener(`click`, function() {
+    startingConditions();
+});
 
-function switchPlayer() {
+const switchPlayer = () => {
     currentScore = 0;
-    document.getElementById(`current--${currentPlayer}`).textContent = currentScore;
-    currentPlayer = currentPlayer === 0 ? 1 : 0;
-    playerZero.classList.toggle(`player--active`);
+    document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    activePlayer = activePlayer === 0 ? 1 : 0;
     playerOne.classList.toggle(`player--active`);
-}
+    playerTwo.classList.toggle(`player--active`);
+};
+
+
+
 
